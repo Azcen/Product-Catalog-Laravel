b12c119bf36da7ed7d2d281e5a7faecf51440b3c
Azcen <alexx1708@gmail.com>
2021-05-19 20:56:26 -0600

    Feat Laravel Passport n Permissions Config

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $data = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+ 
+        if (auth()->attempt($data)) {
+            $token = auth()->user()->createToken('Personal Access Token')->accessToken;
+            return response()->json(['token' => $token], 200);
+        } else {
+            return response()->json(['error' => 'Unauthorised'], 401);
+        }
+    }
+    
+    public function logout(Request $request)
+    {
+        $token = $request->user()->token();
+        $token->revoke();
+        $response = ['message' => 'You have been successfully logged out!'];
+        return response($response, 200);
+    }
+
+    public function user(Request $request)
+    {
+        return response()->json($request->user());
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +14,24 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+Route::group([
+    'prefix' => 'auth',
+], function () {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::group([
+        'middleware' => 'auth:api'
+      ], function() {
+          Route::get('logout',  [AuthController::class, 'logout']);
+          Route::get('user', [AuthController::class, 'user']);
+      });
+});
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware' => ['auth:api'],], function () {
+    // Route::get('vue', 'IndexController@vue');
+    Route::group(['namespace' => 'Resources',], function () {});
+});
+
+
